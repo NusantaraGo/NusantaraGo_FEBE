f46bcb86c1be761c8b9da8a4f4e8f936bed8a935
Salahudin Kholik Prasetyono <Salahudinkoliq10@gmail.com>
2025-05-15 09:42:11 +0700

    Login, SIgnup , navbar , dan footer

--- src/scripts/pages/app.js
+++ src/scripts/pages/app.js
@@ -1,43 +1,52 @@
-import routes from '../routes/routes';
-import { getActiveRoute } from '../routes/url-parser';
+import routes from "../routes/routes";
+import { getActiveRoute } from "../routes/url-parser";
+import { transitionHelper } from "../utils";
+import { getActivePathname } from "../routes/url-parser";
 
 class App {
   #content = null;
-  #drawerButton = null;
-  #navigationDrawer = null;
+  #currentPath;
 
-  constructor({ navigationDrawer, drawerButton, content }) {
+  constructor({ content }) {
     this.#content = content;
-    this.#drawerButton = drawerButton;
-    this.#navigationDrawer = navigationDrawer;
-
-    this._setupDrawer();
-  }
-
-  _setupDrawer() {
-    this.#drawerButton.addEventListener('click', () => {
-      this.#navigationDrawer.classList.toggle('open');
-    });
-
-    document.body.addEventListener('click', (event) => {
-      if (!this.#navigationDrawer.contains(event.target) && !this.#drawerButton.contains(event.target)) {
-        this.#navigationDrawer.classList.remove('open');
-      }
-
-      this.#navigationDrawer.querySelectorAll('a').forEach((link) => {
-        if (link.contains(event.target)) {
-          this.#navigationDrawer.classList.remove('open');
-        }
-      })
-    });
   }
 
   async renderPage() {
     const url = getActiveRoute();
     const page = routes[url];
 
-    this.#content.innerHTML = await page.render();
-    await page.afterRender();
+    // cek trasnition bisa digunakan atau tidak
+    const transition = transitionHelper({
+      /**
+       * Render the page and set the content of #content to the rendered
+       * HTML and call the afterRender method of the page.
+       *
+       * @param {string} url - The URL of the page to render.
+       * @returns {Promise<void>}
+       */
+      updateDOM: async () => {
+        this.#content.innerHTML = await page.render();
+        await page.afterRender();
+      },
+    });
+    // transisition sudah terupdate berhasil
+    transition.updateCallbackDone.then(() => {
+      scrollTo({ top: 0, behavior: "instant" });
+      //akses method private
+      // this.#setupNavigationList();
+    });
+
+    transition.finished.then(() => {
+      // Clear the temporary view-transition-name
+      // if (targetThumbNail) {
+      //   targetThumbNail.style.viewTransitionName = "";
+      // }
+
+      // lanjutkan logic
+
+      // update current path
+      this.#currentPath = getActivePathname();
+    });
   }
 }
 
