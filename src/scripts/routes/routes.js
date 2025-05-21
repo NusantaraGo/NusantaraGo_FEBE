3492111abff6320ae41245d2d755142fc60be350
Fikri <fadilafikri355@gmail.com>
2025-05-22 00:54:21 +0800

    perubahan di file routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,14 +2,14 @@ import HomePage from "../pages/home/home-page";
 import AboutPage from "../pages/about/about-page";
 import LoginPage from "../pages/login/login-page";
 import RegisterPage from "../pages/register/register-page";
-import ProfilePage from "../pages/profile/profile-page";
+import my_profile_page from "../pages/my_profile/my-profile-page";
 
 const routes = {
   "/": new HomePage(),
   "/about": new AboutPage(),
   "/login": new LoginPage(),
   "/register": new RegisterPage(),
-  "/profile": new ProfilePage(),
+  "/profile": new my_profile_page(),
 };
 
 export default routes;
