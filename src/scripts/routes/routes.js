a2b664110b8d6f3f3d6c1a9d4ada911bd51dfbfa
Fikri <fadilafikri355@gmail.com>
2025-05-22 00:43:15 +0800

    percobaan1

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import HomePage from "../pages/home/home-page";
 import AboutPage from "../pages/about/about-page";
 import LoginPage from "../pages/login/login-page";
 import RegisterPage from "../pages/register/register-page";
+import ProfilePage from "../pages/profile/profile-page";
 
 const routes = {
   "/": new HomePage(),
   "/about": new AboutPage(),
   "/login": new LoginPage(),
   "/register": new RegisterPage(),
+  "/profile": new ProfilePage(),
 };
 
 export default routes;
